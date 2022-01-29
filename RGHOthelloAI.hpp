a7db550772a956c04b4e897b1410663f11c09b6c
Rolando Hernandez <rolandoghernandez@outlook.com>
2022-01-29 15:03:52 -0800

    Update RGHOthelloAI.hpp

--- RGHOthelloAI.hpp
+++ RGHOthelloAI.hpp
@@ -10,17 +10,17 @@ namespace rolandoh
 
 	public:
 
-		// main driver function that returns best move possible
+		// Main driver function that returns best move possible.
 		std::pair<int, int> chooseMove(const OthelloGameState& state) override;
 
-		// the recursion function that searches "itself" until the best game state is found
+		// The recursion function that searches "itself" until the best game state is found.
 		int search(std::unique_ptr<OthelloGameState>& s, int depth, bool color);
 
-		// evaluation function to determine best state, NOT the state itself
+		// Evaluation function to determine best state, NOT the state itself.
 		int evaluation(std::unique_ptr<OthelloGameState>& state);
 
 	private:
-		// Nodes for a linked list
+		// Nodes for a linked list.
 		struct Node
 		{
 			Node* next;
